2bf29ee478a653b353442b6c4631692a31b1a9c5
TexTrue <3140846162@qq.com>
2023-04-25 15:39:15 +0800

    ConfigScreen

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -1,25 +1,29 @@
 package fi.dy.masa.litematica;
 
+import fi.dy.masa.litematica.gui.GuiConfigs;
+import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.fabricmc.api.ModInitializer;
 import fi.dy.masa.malilib.event.InitializationHandler;
 import fi.dy.masa.litematica.config.Configs;
 
-public class Litematica implements ModInitializer
-{
+@Mod(Reference.MOD_ID)
+public class Litematica {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
-    @Override
-    public void onInitialize()
-    {
+    public Litematica() {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+
+        ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
+            GuiConfigs gui = new GuiConfigs();
+            gui.setParent(screen);
+            return gui;
+        });
     }
 
-    public static void debugLog(String msg, Object... args)
-    {
-        if (Configs.Generic.DEBUG_LOGGING.getBooleanValue())
-        {
+    public static void debugLog(String msg, Object... args) {
+        if (Configs.Generic.DEBUG_LOGGING.getBooleanValue()) {
             Litematica.logger.info(msg, args);
         }
     }

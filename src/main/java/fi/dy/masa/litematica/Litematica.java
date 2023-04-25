78358eadada9b1794e7d76c28adeb09c3f2d6422
TexTrue <3140846162@qq.com>
2023-04-25 14:52:08 +0800

    ConfigScreen

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -1,17 +1,24 @@
 package fi.dy.masa.litematica;
 
+import fi.dy.masa.litematica.gui.GuiConfigs;
+import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
 import net.fabricmc.api.ModInitializer;
 
-public class Litematica implements ModInitializer
-{
+@Mod(Reference.MOD_ID)
+public class Litematica {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
-    @Override
-    public void onInitialize()
-    {
+    public Litematica() {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+
+        ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
+            GuiConfigs gui = new GuiConfigs();
+            gui.setParent(screen);
+            return gui;
+        });
     }
 }

db1570303e1271b8524bdb39faccd8655a4c4ec3
TexTrue <3140846162@qq.com>
2023-05-24 15:15:52 +0800

    litemetica -> forgemetica

--- src/main/java/fi/dy/masa/litematica/Reference.java
+++ src/main/java/fi/dy/masa/litematica/Reference.java
@@ -5,6 +5,6 @@ import fi.dy.masa.malilib.util.StringUtils;
 public class Reference
 {
     public static final String MOD_ID = "litematica";
-    public static final String MOD_NAME = "Litematica";
+    public static final String MOD_NAME = "Forgematica";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }

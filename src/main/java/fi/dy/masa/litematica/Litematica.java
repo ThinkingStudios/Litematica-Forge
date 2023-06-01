2ef7914ffef180522738d6961a2b2e1e2a5bece2
TexTrue <3140846162@qq.com>
2023-06-01 17:14:33 +0800

    fixed #7

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -2,9 +2,11 @@ package fi.dy.masa.litematica;
 
 import fi.dy.masa.litematica.gui.GuiConfigs;
 import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
+@Mod(Reference.MOD_ID)
 public class Litematica {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 

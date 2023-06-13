9e334801859ba2f24b47845895fd9c172b31ac2e
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-06-13 18:34:54 +0800

    fix server incompatibility

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -2,7 +2,10 @@ package fi.dy.masa.litematica;
 
 import fi.dy.masa.litematica.gui.GuiConfigs;
 import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
@@ -11,8 +14,14 @@ public class Litematica {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
     public Litematica() {
-        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        modEventBus.addListener(this::onInitializeClient);
+    }
+
+    public void onInitializeClient(FMLClientSetupEvent event) {
+        ForgePlatformCompat.getInstance().getModClientExtensionPoint();
+        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
         ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
             GuiConfigs gui = new GuiConfigs();
             gui.setParent(screen);

fc2b7df6e478df13f7501cd4b76385f3e4f92f37
TexTrue <3140846162@qq.com>
2024-01-18 18:24:57 +0800

    fix icon missing, update mafglib to `0.1.8`

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -1,7 +1,7 @@
 package fi.dy.masa.litematica;
 
 import fi.dy.masa.litematica.gui.GuiConfigs;
-import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import fi.dy.masa.malilib.compat.forge.ForgePlatformUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -22,9 +22,9 @@ public class Litematica {
     }
 
     public void onInitializeClient(FMLClientSetupEvent event) {
-        ForgePlatformCompat.getInstance().getModClientExtensionPoint();
+        ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-        ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
+        ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
             GuiConfigs gui = new GuiConfigs();
             gui.setParent(screen);
             return gui;

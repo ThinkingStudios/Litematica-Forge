51ef239561d740169c9887affd71b8cf76145bb4
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-05-01 21:30:30 +0800

    clean

--- src/main/java/fi/dy/masa/litematica/mixin/compat/MixinModelDataManager.java
+++ src/main/java/fi/dy/masa/litematica/mixin/compat/MixinModelDataManager.java
@@ -12,16 +12,17 @@ import fi.dy.masa.litematica.world.WorldSchematic;
 public class MixinModelDataManager {
 
     /**
+     * if we don't catch this Forge does stupid things
+     * it calls requestModelData on any client world when adding a BlockEntity
+     * but if it's not mc.world it crashes because model data may only
+     * be used on the current client world
+     *
      * @author ZacSharp
-     * @reason Fix about Block's ModelData issues. <a href="https://github.com/ThinkingStudios/Litematica-Forge/issues/48">ThinkingStudios/Litematica-Forge/#48</a>
-     * @see <a href="https://github.com/ZacSharp/litematica-forge/commit/73c96536d34bbf0612e099211c96ea77bec5e335#diff-99a005b1c70ed23a8a0ff36c1d1bd16d13a194a96e5d1cc0fb8d5eaf77098717">ZacSharp/litematica-forge/commit/73c9653</a>
+     * @reason Fix about Forge's ModelData issues. See: <a href="https://github.com/ThinkingStudios/Litematica-Forge/issues/48">ThinkingStudios/Litematica-Forge issue#48</a>
+     * @see <a href="https://github.com/ZacSharp/litematica-forge/commit/73c96536d34bbf0612e099211c96ea77bec5e335#diff-99a005b1c70ed23a8a0ff36c1d1bd16d13a194a96e5d1cc0fb8d5eaf77098717">ZacSharp/litematica-forge commit#73c9653</a>
      */
     @Inject(method = "requestModelDataRefresh", at = @At("HEAD"), cancellable = true, remap = false)
     private static void requestModelDataRefresh(BlockEntity blockEntity, CallbackInfo cir) {
-        // if we don't catch this Forge does stupid things
-        // it calls requestModelData on any client world when adding a te
-        // but if it's not mc.world it crashes because model data may only
-        // be used on the current client world
         if (blockEntity.getWorld() instanceof WorldSchematic) {
             cir.cancel();
         }

4ea3f17aba571aff93eabfe4b5cf3e19951b5c05
TexTrue <3140846162@qq.com>
2024-07-29 14:46:15 +0800

    update MaFgLib

--- src/main/java/org/thinkingstudio/forgematica/Forgematica.java
+++ src/main/java/org/thinkingstudio/forgematica/Forgematica.java
@@ -4,17 +4,22 @@ import fi.dy.masa.litematica.InitHandler;
 import fi.dy.masa.litematica.Reference;
 import fi.dy.masa.litematica.gui.GuiConfigs;
 import fi.dy.masa.malilib.event.InitializationHandler;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLLoader;
-import org.thinkingstudio.mafglib.util.ForgePlatformUtils;
+import org.thinkingstudio.mafglib.util.ForgeUtils;
 
 @Mod(Reference.MOD_ID)
 public class Forgematica {
     public Forgematica() {
         if (FMLLoader.getDist().isClient()) {
-            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
+            ModContainer modContainer = ModLoadingContext.get().getActiveContainer();
+
+            ForgeUtils.getInstance().getClientModIgnoredServerOnly(modContainer);
             InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
+
+            ForgeUtils.getInstance().registerModConfigScreen(modContainer, (screen) -> {
                 GuiConfigs gui = new GuiConfigs();
                 gui.setParent(screen);
                 return gui;

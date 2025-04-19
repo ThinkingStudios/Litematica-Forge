88b2b6d92810d7553b43af76193e95f0539b9553
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-04-19 11:10:57 +0800

    clean

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -4,14 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
 import fi.dy.masa.litematica.config.Configs;
-import org.thinkingstudio.mafglib.loader.entrypoints.ModInitializer;
 
-public class Litematica implements ModInitializer
+public class Litematica
 {
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
 
-    @Override
-    public void onInitialize()
+    public static void onInitialize()
     {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
     }
--- src/main/java/org/thinkingstudio/forgematica/Forgematica.java
+++ src/main/java/org/thinkingstudio/forgematica/Forgematica.java
@@ -10,15 +10,13 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 
 import org.thinkingstudio.mafglib.loader.entrypoints.ConfigScreenEntrypoint;
-import org.thinkingstudio.mafglib.loader.entrypoints.ModInitializer;
 
 @Mod(value = Reference.MOD_ID, dist = Dist.CLIENT)
 public class Forgematica {
     public Forgematica(ModContainer modContainer) {
         if (FMLLoader.getDist().isClient()) {
-//            modContainer.registerExtensionPoint(ConfigScreenEntrypoint.class, new ModMenuImpl());
-//            modContainer.registerExtensionPoint(ModInitializer.class, new Litematica());
-            new Litematica().onInitialize();
+            modContainer.registerExtensionPoint(ConfigScreenEntrypoint.class, new ModMenuImpl());
+            Litematica.onInitialize();
         }
     }
 }

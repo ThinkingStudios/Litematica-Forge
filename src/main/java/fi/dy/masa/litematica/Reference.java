60cc4f38e9b0c2d9d25cc4772a6f1ccbe97bcec6
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-07-14 16:06:53 +0800

    Update Reference.java

--- src/main/java/fi/dy/masa/litematica/Reference.java
+++ src/main/java/fi/dy/masa/litematica/Reference.java
@@ -10,6 +10,6 @@ public class Reference
     public static final String MOD_NAME = "Litematica";
     public static final String MOD_VERSION = StringUtils.getModVersionString(PORT_ID);
     public static final String MC_VERSION = MinecraftVersion.CURRENT.getName();
-    public static final String MOD_TYPE = "fabric";
+    public static final String MOD_TYPE = "neoforge";
     public static final String MOD_STRING = MOD_ID+"-"+MOD_TYPE+"-"+MC_VERSION+"-"+MOD_VERSION;
 }
